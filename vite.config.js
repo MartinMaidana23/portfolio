d00976c0303be81353d7eaddb966d5c69f5f549c
MartinMaidana23 <martinmaidana90@gmail.com>
2023-05-30 19:55:10 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import sassPlugin from 'vite-plugin-sass';
-import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     sassPlugin()
-    
   ],
 })
 
