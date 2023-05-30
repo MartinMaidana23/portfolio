57d17cc0a4aa9fed7d291d0dff340abb80f5b7fb
MartinMaidana23 <martinmaidana90@gmail.com>
2023-05-30 20:01:41 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import sassPlugin from 'vite-plugin-sass';
+import sassDts from 'vite-plugin-sass-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    sassPlugin()
+    sassDts({
+      enabledMode: ['development', 'production'],
+      global: {
+        generate: true,
+        outFile: path.resolve(__dirname, './src/style.d.ts'),
+      },
+      sourceDir: path.resolve(__dirname, './src'),
+      outDir: path.resolve(__dirname, './dist'),
+    }),
   ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles" as common;`,
+        importer(...args) {
+          if (args[0] !== '@/styles') {
+            return
+          }
+
+          return {
+            file: `${path.resolve(__dirname, './src/assets/styles')}`,
+          }
+        },
+      },
+    },
+  },
 })
 
 
