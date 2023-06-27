8d64858003a2619863a763d99ab31f921a8e964f
MartinMaidana23 <martinmaidana90@gmail.com>
2023-06-27 20:01:34 -0300

    form done

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './form.css'
 import Input from './Input'
 import useForm from '../hooks/useForm'
 import ClipLoader from 'react-spinners/ClipLoader'
@@ -12,8 +11,8 @@ const Form = (props) => {
 
 
   return (
-    <div className='form__container' >
-        <form className='form' onSubmit={handleSubmit}>
+    <div className='form__container w-4/5 my-12 mx-3' >
+        <form className='form flex justify-center items-center flex-col' onSubmit={handleSubmit}>
           {Object.entries(initialForm).map(([key], index)=>(
             <Input
               type={key==='text' ? 'text' : key==='email' ? 'email' : key==='phone' ? 'number' : 'text'}
@@ -25,7 +24,7 @@ const Form = (props) => {
           ))}
           <Modal display={modal ? 'block' : 'none'} message={lang==='en'?'Message sent successfully!':'Mensaje enviado con exito!'}/>
           
-          <button className='btn'>{!loading && (lang==='en'?'SEND MESSAGE':'ENVIAR MENSAJE')} {loading && <ClipLoader color={'#4ee1a0'}/>}</button>
+          <button className='btn bg-none m-4 no-underline border-solid border-b-2 hover:cursor-pointer hover:-translate-y-1 transition-all duration-300 md:text-lg xl:text-2xl active:translate-y-1'>{!loading && (lang==='en'?'SEND MESSAGE':'ENVIAR MENSAJE')} {loading && <ClipLoader color={'#4ee1a0'}/>}</button>
         </form>
     </div>
   )
--- src/components/form/Input.jsx
+++ src/components/form/Input.jsx
@@ -4,7 +4,7 @@ const Input = (props) => {
     const {type, name, placeholder, onChange} = props
   return (
     <>
-        <input type={type} name={name} placeholder={placeholder} className='form__input' onChange={onChange} />
+        <input type={type} name={name} placeholder={placeholder} className='form__input w-full mb-5 text-base border-solid border-b-2 border-white p-4 bg-transparent focus:outline-none font-sans font-medium md:text-base xl:text-2xl' onChange={onChange} />
     </>
   )
 }
