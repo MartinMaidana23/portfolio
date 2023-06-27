2c353ba0ad0a6869be98f7955fb75d131022dace
MartinMaidana23 <martinmaidana90@gmail.com>
2023-06-27 18:16:16 -0300

    contact done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,5 @@
-import React, {useEffect, useRef} from 'react'
-import './contact.css'
+import React from 'react'
 import Form from '../form/Form'
-import { gsap } from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import useGsap from '../hooks/useGsap'
 
 const Contact = (props) => {
@@ -33,10 +30,20 @@ const Contact = (props) => {
 )
 
   return (
-    <div className={`contact`} id='contact' ref={refEl}>
-        <div className={`title`}>
-            <h2>{lang==='en'?'Contact':'Contacto'}</h2>
-            <p>{lang==='en'?'I would love to hear about your project and how I could help. Please fill in the form, and I’ll get back to you as soon as possible.' :'Me encantaria escuchar sobre tu proyecto y como podria ayudar, por favor llena el formulario y te voy a contestar lo mas rapido posible' }</p>
+    <div className=
+                {`
+
+contact bg-[#242424] w-screen h-screen flex items-center justify-center flex-col
+                
+                `} 
+    id='contact' ref={refEl}>
+        <div className=
+                      {`
+                      title w-[90%] mt-7 flex flex-col items-center xl:w-3/5 xl:justify-center xl:items-center
+                      `}
+        >
+            <h2 className='text-4xl font-bold md:text-7xl xl:text-[88px]' >{lang==='en'?'Contact':'Contacto'}</h2>
+            <p className='font-normal text-base text-center text-[#d9d9d9] md:text-lg xl:text-2xl w-3/5' >{lang==='en'?'I would love to hear about your project and how I could help. Please fill in the form, and I’ll get back to you as soon as possible.' :'Me encantaria escuchar sobre tu proyecto y como podria ayudar, por favor llena el formulario y te voy a contestar lo mas rapido posible' }</p>
         </div>
         {<Form initialForm={initialForm} lang={lang} />}
     </div>
